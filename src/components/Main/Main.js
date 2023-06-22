c842152791735647b3a0049eb78f3b666a19d4e2
Hisime <rihisime@gmail.com>
2023-06-23 01:38:33 +0400

    tech block

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import './Main.css';
 import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs";
 
 function Main() {
     return (
     <main>
         <AboutProject/>
+        <Techs/>
     </main>
     );
 }
