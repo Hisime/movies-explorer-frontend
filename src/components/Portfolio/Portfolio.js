760e34e918b59ac135b40c500ba4966b99438ebe
Hisime <rihisime@gmail.com>
2023-06-24 20:56:55 +0400

    Portfolio block

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,7 +7,7 @@ function Portfolio() {
             <ul className="portfolio__list">
                 <li className="portfolio__list-item">
                     <span className="portfolio__list-name">Статичный сайт</span>
-                    <a className="portfolio__link" href="#" target="_blank">
+                    <a className="portfolio__link" href="https://github.com/Hisime/how-to-learn" target="_blank">
                         <svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M2.60653 16.5241L14.9645 4.14489L14.9432 13.6903H17.2656V0.181818H3.77841L3.7571 2.48295H13.3026L0.944603 14.8622L2.60653 16.5241Z" fill="black"/>
                         </svg>
@@ -15,7 +15,7 @@ function Portfolio() {
                 </li>
                 <li className="portfolio__list-item">
                     <span className="portfolio__list-name">Адаптивный сайт</span>
-                    <a className="portfolio__link" href="#" target="_blank">
+                    <a className="portfolio__link" href="https://github.com/Hisime/russian-travel" target="_blank">
                         <svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M2.60653 16.5241L14.9645 4.14489L14.9432 13.6903H17.2656V0.181818H3.77841L3.7571 2.48295H13.3026L0.944603 14.8622L2.60653 16.5241Z" fill="black"/>
                         </svg>
@@ -23,7 +23,7 @@ function Portfolio() {
                 </li>
                 <li className="portfolio__list-item">
                     <span className="portfolio__list-name">Одностраничное приложение</span>
-                    <a className="portfolio__link" href="#" target="_blank">
+                    <a className="portfolio__link" href="https://hisime.github.io/mesto/" target="_blank">
                         <svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M2.60653 16.5241L14.9645 4.14489L14.9432 13.6903H17.2656V0.181818H3.77841L3.7571 2.48295H13.3026L0.944603 14.8622L2.60653 16.5241Z" fill="black"/>
                         </svg>
