8b8a3d4db93831172f7cd00f71a1f94b43558b09
Hisime <rihisime@gmail.com>
2023-07-01 17:34:46 +0400

    Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,27 @@
 import './Login.css';
+import Logo from "../Logo/Logo";
+import FormTitle from "../FormTitle/FormTitle";
+import FormInput from "../FormInput/FormInput";
+import FormButton from "../FormButton/FormButton";
 
 function Login() {
     return (
-        <div>Register</div>
+        <main className="register">
+            <section className="register-section">
+                <Logo/>
+                <FormTitle text="Рады видеть!"/>
+                <FormInput text="E-mail" type="email"/>
+                <FormInput text="Пароль" type="password"/>
+                <div className="register-section__bottom">
+                    <FormButton text="Войти"/>
+                    <div className="register-section__tip">
+                        <span className="register-section__tip-text">Ещё не зарегистрированы?</span>
+                        <a href="/sign-up" className="block-link">Регистрация</a>
+                    </div>
+                </div>
+
+            </section>
+        </main>
     );
 }
 
