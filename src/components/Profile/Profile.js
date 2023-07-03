39fcd97a0c39a7557ff236ffd75c22a39d49e864
Hisime <rihisime@gmail.com>
2023-07-03 11:10:48 +0400

    Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,29 @@
 import './Profile.css';
+import Header from "../Header/Header";
+import FormTitle from "../FormTitle/FormTitle";
 
 function Profile() {
     return (
-        <div>Profile</div>
+        <>
+            <Header isLogged={true}/>
+            <main className="profile">
+                <FormTitle text="Привет, Виталий!"></FormTitle>
+                <div className="profile__inputs">
+                    <div className="profile__input-wrapper">
+                        <label className="profile__label" htmlFor="">Имя</label>
+                        <input className="profile__input" type="text" value="Виталий" disabled/>
+                    </div>
+                    <div className="profile__input-wrapper">
+                        <label className="profile__label" htmlFor="">E-mail</label>
+                        <input className="profile__input" type="text" value="pochta@yandex.ru" disabled/>
+                    </div>
+                </div>
+                <div className="profile__buttons">
+                    <button className="profile__button">Редактировать</button>
+                    <button className="profile__button profile__button--danger">Выйти из аккаунта</button>
+                </div>
+            </main>
+        </>
     );
 }
 
